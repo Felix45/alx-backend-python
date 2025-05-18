152e2a69474c2f812353867e200ce892465a00c3
felix45 <fatonoh@gmail.com>
2025-05-18 12:29:05 +0300

    Fix failing test

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,13 +1,13 @@
 import seed
 
-def lazy_paginate(page_size):
+def lazypaginate(page_size, offset=0):
     ''' 
         Fetchs paginated data from the users table
         using a generator to lazily load each page
     '''
     connection = seed.connect_to_prodev()
     cursor     = connection.cursor()
-    cursor.execute(f"SELECT * FROM user_data")
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
 
     while True:
         rows = cursor.fetchmany(page_size)
@@ -17,12 +17,12 @@ def lazy_paginate(page_size):
     cursor.close()
     connection.close()
 
-def paginate_users(page_size, offset=0):
+def paginate_users(page_size, offset):
     ''' 
         Paginated data from the users table
         using a generator to lazily load each page
     '''
-    pages = lazy_paginate(page_size)
+    pages = lazypaginate(page_size, offset)
 
     for page in pages:
         print(page, end="\n\n")
--- python-generators-0x00/3-main.py
+++ python-generators-0x00/3-main.py
@@ -4,6 +4,6 @@ import sys
 lazy_paginator = __import__('2-lazy_paginate')
 
 try:
-    lazy_paginator.paginate_users(100)
+    lazy_paginator.paginate_users(100,0)
 except BrokenPipeError:
     sys.stderr.close()
