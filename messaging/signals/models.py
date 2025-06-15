87c464a478ac4c3a918c421bdc3ea9b316dbdf21
felix45 <fatonoh@gmail.com>
2025-06-15 18:35:49 +0300

    Rename recipient to receiver

--- messaging/signals/models.py
+++ messaging/signals/models.py
@@ -5,12 +5,12 @@ class Message(models.Model):
     ''' A model representing a message in the system. '''
 
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    recipient = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Message from {self.sender.username} to {self.recipient.username} at {self.timestamp}'
+        return f'Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}'
 
 
 class Notification(models.Model):
