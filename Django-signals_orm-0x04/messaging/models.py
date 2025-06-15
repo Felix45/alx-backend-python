1c4bdcc90100c88fc11aee43e70d49c3ff23c46f
felix45 <fatonoh@gmail.com>
2025-06-15 21:37:59 +0300

    Fix failing check

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,7 @@ class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
+    edited  = models.BooleanField(default=False, help_text="Indicates if the message has been edited.")
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -23,4 +24,15 @@ class Notification(models.Model):
 
     def __str__(self):
         return f'Notification for {self.user.username} regarding message from {self.message.sender.username}'
+    
+class MessageHistory(models.Model):
+    ''' A model representing the history of a message. '''
+
+    message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
+    content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, related_name='edited_messages', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'History for message {self.message.id} at {self.edited_at} {self.edited_by.username if self.edited_by else "System"}'
 
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -11,3 +11,12 @@ def create_notification(sender, instance, created, **kwargs):
             message=instance
         )
         print(f'Notification created for {instance.recipient.username} regarding new message from {instance.sender.username}.')
+
+@receiver(pre_save, sender=Message)
+def create_message_history(sender, instance, **kwargs):
+    ''' Signal to create a message history entry when a message is about to be saved. '''
+    if instance.id:
+        MessageHistory.objects.create(
+            message=instance,
+            content=sender.objects.get(id=instance.id).content if instance.content else ''
+        )
